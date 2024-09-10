cd0e12d075ceddb0dfc3a85135688c2c577d4750
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-09-10 11:10:47 +0200

    Klimadata.py added

---
+++ Klimadata.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Tilgå kode gemt i st.secrets
+kode = st.secrets["general"]["code"]
+
+# Brug koden i din app
+st.title('Brug af hemmelig kode i Streamlit')
+
+if kode == "din_hemmelige_kode_her":
+    st.write("Koden er korrekt!")
+else:
+    st.write("Forkert kode!")
