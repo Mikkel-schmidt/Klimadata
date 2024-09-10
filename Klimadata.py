6fd9b6fbe7d90dbc8d8e8c040663db627c0de368
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-09-10 11:33:17 +0200

    add

--- Klimadata.py
+++ Klimadata.py
@@ -7,13 +7,14 @@ from streamlit_functions import check_password
 if check_password():
     st.success('Login success')
 
-    styles_layers = pd.read_csv('layers_and_styles.csv', header=True, index=False)
-    
+    layers_styles = pd.read_csv('layers_and_styles.csv', header=True, index=False)
+    st.write(layers_styles.head())
+
     # Create a selectbox for layers
-    selected_layer = st.selectbox('Choose a layer', styles_layers['layer_name'].unique())
+    selected_layer = st.selectbox('Choose a layer', layers_styles['layer_name'].unique())
 
     # Filter the styles based on selected layer
-    filtered_styles = styles_layers[styles_layers['layer_name'] == selected_layer]['style']
+    filtered_styles = layers_styles[layers_styles['layer_name'] == selected_layer]['style']
 
     # Create a selectbox for styles based on the selected layer
     selected_style = st.selectbox('Choose a style', filtered_styles)
