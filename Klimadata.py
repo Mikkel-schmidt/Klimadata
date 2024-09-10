5e528baa8d396a4f5b9b2ddc6b5986a4c374f786
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-09-10 12:50:13 +0200

    Adresse

--- Klimadata.py
+++ Klimadata.py
@@ -1,13 +1,47 @@
 import streamlit as st
 import pandas as pd
+import folium
+from geopy.geocoders import Nominatim
 
 from streamlit_functions import check_password
+
+st.set_page_config(layout="wide", page_title="Forside")
 
 
 if check_password():
     st.success('Login success')
 
-    st.write('Hej')
+    # Opret en geokodningsfunktion ved hjælp af Nominatim
+    geolocator = Nominatim(user_agent="geoapiExercises")
+
+    # Angiv adressen
+    adresse = st.text_input("Skriv adresse", value="Kongens Nytorv 34, 1050 København")
+
+    # Geokod adressen (find koordinaterne)
+    location = geolocator.geocode(adresse)
+
+    # Tjek om geokodningen lykkedes
+    if location:
+        # Print latitude og longitude
+        st.success('Adresse fundet')
+        print(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
+        
+        # Opret et Folium-kort centreret på de fundne koordinater
+        m = folium.Map(location=[location.latitude, location.longitude], zoom_start=15, crs='EPSG3857')  # EPSG3857 bruges af standard webkort
+        
+        # Tilføj en markør ved den fundne adresse
+        folium.Marker([location.latitude, location.longitude], popup=adresse).add_to(m)
+        
+        # Gem kortet som en HTML-fil og vis det
+        m.save('adresse_geokodning_kort.html')
+        
+        # Hvis du kører det i en Jupyter Notebook, kan du vise kortet direkte med:
+        m
+    else:
+        st.write("Kunne ikke finde den angivne adresse.")
+
+
+
 
     layers_styles = pd.read_csv('https://raw.githubusercontent.com/Mikkel-schmidt/Klimadata/master/layers_and_styles.csv')
     st.write(layers_styles.head())
