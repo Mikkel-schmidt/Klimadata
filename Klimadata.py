2a41fb739cb735a76e9ff67907cd21df66120c83
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-09-11 10:07:33 +0200

    reload map

--- Klimadata.py
+++ Klimadata.py
@@ -147,61 +147,62 @@ if check_password():
         st_folium(m2, width=1200, height=700)
 
     with tab3:
-        # Create a selectbox for layers (frontend-friendly names)
-        selected_layer_name = 'Flow ekstremregn' #= tab1.selectbox('Vælg et lag', layers_styles['layer_name'].unique(), index=2, key="layer_select")
+        if st.button("Reload Havvand Map"):
+            # Create a selectbox for layers (frontend-friendly names)
+            selected_layer_name = 'Flow ekstremregn' #= tab1.selectbox('Vælg et lag', layers_styles['layer_name'].unique(), index=2, key="layer_select")
 
-        # Find de tilsvarende lag-værdier (backend values) baseret på valgte lag-navn
-        selected_layer_value = layers_styles[layers_styles['layer_name'] == selected_layer_name]['layer_value'].iloc[0]
+            # Find de tilsvarende lag-værdier (backend values) baseret på valgte lag-navn
+            selected_layer_value = layers_styles[layers_styles['layer_name'] == selected_layer_name]['layer_value'].iloc[0]
 
-        # Filter stilarterne baseret på valgte lag
-        filtered_styles = layers_styles[layers_styles['layer_name'] == selected_layer_name]
+            # Filter stilarterne baseret på valgte lag
+            filtered_styles = layers_styles[layers_styles['layer_name'] == selected_layer_name]
 
-        # Create a selectbox for styles (frontend-friendly names)
-        selected_style_name = 'Flyderetning af vand'#st.selectbox('Vælg en ekstremregnsmængde', filtered_styles['style_name'], index=0, key="style_select_rain")
+            # Create a selectbox for styles (frontend-friendly names)
+            selected_style_name = 'Flyderetning af vand'#st.selectbox('Vælg en ekstremregnsmængde', filtered_styles['style_name'], index=0, key="style_select_rain")
 
-        # Find den tilsvarende style-værdi (backend value) baseret på valgt stil-navn
-        selected_style_value = filtered_styles[filtered_styles['style_name'] == selected_style_name]['style_value'].iloc[0]
+            # Find den tilsvarende style-værdi (backend value) baseret på valgt stil-navn
+            selected_style_value = filtered_styles[filtered_styles['style_name'] == selected_style_name]['style_value'].iloc[0]
 
-        # Display selected options
-        # st.write(f'Valgt lag: {selected_layer_name}')
-        # st.write(f'Valgt stil: {selected_style_name}')
+            # Display selected options
+            # st.write(f'Valgt lag: {selected_layer_name}')
+            # st.write(f'Valgt stil: {selected_style_name}')
 
-        # st.write(f'Valgt lag: {selected_layer_value}')
-        # st.write(f'Valgt stil: {selected_style_value}')
+            # st.write(f'Valgt lag: {selected_layer_value}')
+            # st.write(f'Valgt stil: {selected_style_value}')
 
-        # Opret et Folium-kort centreret på den fundne adresse eller fallback-location
-        m3 = folium.Map(location=[latitude, longitude], zoom_start=15, crs='EPSG3857')
+            # Opret et Folium-kort centreret på den fundne adresse eller fallback-location
+            m3 = folium.Map(location=[latitude, longitude], zoom_start=15, crs='EPSG3857')
 
-        # Tilføj en markør ved den fundne adresse, hvis tilgængelig
-        if location:
-            folium.Marker([latitude, longitude], popup=adresse).add_to(m3)
+            # Tilføj en markør ved den fundne adresse, hvis tilgængelig
+            if location:
+                folium.Marker([latitude, longitude], popup=adresse).add_to(m3)
 
-        # WMS-serverens URL
-        wms_url = 'https://api.dataforsyningen.dk/dhm?service=WMS&request=GetCapabilities&token=' + st.secrets['token']
+            # WMS-serverens URL
+            wms_url = 'https://api.dataforsyningen.dk/dhm?service=WMS&request=GetCapabilities&token=' + st.secrets['token']
 
-        # Tilføj et baselayer
-        folium.TileLayer('CartoDB positron', name="CartoDB Positron").add_to(m3)
+            # Tilføj et baselayer
+            folium.TileLayer('CartoDB positron', name="CartoDB Positron").add_to(m3)
 
-        # Tilføj WMS-lag med backend-lag og stil værdier
-        folium.raster_layers.WmsTileLayer(
-            url=wms_url,
-            name=f"{selected_style_name}",  # Navn der vises i lagvælgeren (frontend-friendly)
-            layers=selected_layer_value,  # Navn på WMS-laget (backend value)
-            styles=selected_style_value,  # Style for WMS-laget (backend value)
-            fmt='image/png',  # Billedformat
-            transparent=True,  # Transparent baggrund
-            version='1.1.1',  # WMS version
-            overlay=True,  # Sæt overlay til True
-            control=True,  # Vis kontrolelement for at vælge lag
-            show=True,
-        ).add_to(m3)
+            # Tilføj WMS-lag med backend-lag og stil værdier
+            folium.raster_layers.WmsTileLayer(
+                url=wms_url,
+                name=f"{selected_style_name}",  # Navn der vises i lagvælgeren (frontend-friendly)
+                layers=selected_layer_value,  # Navn på WMS-laget (backend value)
+                styles=selected_style_value,  # Style for WMS-laget (backend value)
+                fmt='image/png',  # Billedformat
+                transparent=True,  # Transparent baggrund
+                version='1.1.1',  # WMS version
+                overlay=True,  # Sæt overlay til True
+                control=True,  # Vis kontrolelement for at vælge lag
+                show=True,
+            ).add_to(m3)
 
-        # Tilføj kontrolpanel til at vælge mellem lagene
-        folium.LayerControl(position='topright', collapsed=False).add_to(m3)
+            # Tilføj kontrolpanel til at vælge mellem lagene
+            folium.LayerControl(position='topright', collapsed=False).add_to(m3)
 
-        
-        # Vis kortet i Streamlit og opdater det dynamisk
-        st_folium(m3, width=1200, height=700)
+            
+            # Vis kortet i Streamlit og opdater det dynamisk
+            st_folium(m3, width=1200, height=700)
 
     with tab4:
         # Create a selectbox for layers (frontend-friendly names)
