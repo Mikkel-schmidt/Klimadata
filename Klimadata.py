46064ad742b530b4912d2f74f03326a41f9f2a32
Mikkel Rasmus Schmidt <Mikkel.schmidt@hotmail.com>
2024-12-11 13:43:06 +0100

    1,6

--- Klimadata.py
+++ Klimadata.py
@@ -537,7 +537,7 @@ if check_password():
                 
                 # Vis label og billede
                 if label:
-                    col11, col22 = col2.columns([1, 5])
+                    col11, col22 = col2.columns([1, 6])
                     col11.image(image_url)
                     col22.write(f"{label}")
 
